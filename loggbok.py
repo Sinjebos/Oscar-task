c3a17fab4429902d49908e0eba0301b2da006f98
Sinjebos <Nicke_95_@hotmail.com>
2020-12-06 14:58:35 +0100

    updated filebook

--- loggbok.py
+++ loggbok.py
@@ -1,60 +1,53 @@
 import time
 import datetime
-from bok import bok
 import os
 
-datum = datetime.datetime.now()
-loggtitel = ""
-loggtext = ""
-my_list = []
-def logg():
-    nyfil = input("Skapa ny fil eller använd en existerande fil för spara din logg\nAvsluta filnamnet med .txt\nFil: ")
-    if nyfil.endswith(".txt"):
-        loggtitel = input("Titel till din logg: ").upper()
-        loggtext = input("Implementera vad du vill i din logg: ")
-        my_list.append(loggtitel + " " + loggtext + " " + str(datum))
-        myfile = open(nyfil, 'a')
-        myfile.write("\n" + loggtitel + "\n" + loggtext + "\n" + str(datum))
-        print(my_list.count(loggtitel))
-    else:
-        print("Avsluta filnamnet med .txt")
+date = datetime.datetime.now()
+def newFile():
+    new_file = input("Create a new file or use a file that already excist\nFile: ") + ".txt"
+    file_title = input("Title for you file: ").upper()
+    file_text = input("Add what you want to your file: ")
+    my_file = open(new_file, 'a')
+    my_file.write("\n" + file_title + "\n" + file_text + "\n" + str(date))
 
-def taBort():
-    tabortfil = input("Vilken logg vill du ta bort?\nTitel: ")
-    if tabortfil.endswith(".txt"):
-        os.remove(tabortfil)
+def removeFile():
+    remove_file = input("Which file would you like to remove?\nTitle: ") + ".txt"
+    if remove_file.endswith(".txt"):
+        os.remove(remove_file)
     else:
-        print("Kan inte ta bort den filen!")
+        print("Can't remove that file")
 
 
 def printallLogs():
-    #lasfil = input("Vilken fil vill du läsa?\nFil:: ")
-    #if:
-     #   myfile = open(lasfil, 'r')
-      #  print(myfile.read())
+    read_all_files = ""
+    if read_all_files.endswith(".txt"):
+        my_file = open(read_all_files, 'r')
+        print(my_file.read())
 
 def searchForLogg():
-    lasfil = input("Vilken fil vill du läsa?\nFil:: ")
-    if lasfil.endswith(".txt"):
-        myfile = open(lasfil, 'r')
-        print(myfile.read())
-     else:
-        print("Du har ingen fil sparad vid det namnet eller avsluta inte med .txt")
+    read_file = input("What file would you like to search for?\nFile:: ") + ".txt"
+    if read_file.endswith(".txt"):
+        my_file = open(read_file, 'r')
+        print(my_file.read())
+    else:
+        print("You dont have any file with that name")
 
-def welcomeLogg():
-    print("Välkommen till din virtuella loggbok")
-    isRunning = True
-    while isRunning:
-        sok = input("[1] Skapa en logg\n[2] Sök bland existerande loggar\n[3] Printa alla sparade loggar\n[4] Ta bort en sparad logg\n[5] Stäng loggboken\nOption:: ")
-        if sok == "1":
-            logg()
-        elif sok == "2":
+
+print("Welcome to your virtual filebook")
+isRunning = True
+while isRunning:
+    try:
+        user_input = input("[1] Create a file\n[2] Search for a file that already excist\n[3] Print all saved files\n[4] Remove a saved file\n[5] Close your filebook\nOption:: ")
+        if user_input == "1":
+            newFile()
+        elif user_input == "2":
             searchForLogg()
-        elif sok == "3":
+        elif user_input == "3":
             printallLogs()
-        elif sok == "4":
-            taBort()
-        elif sok == "5":
-            print("Stänger loggboken ha de gött")
+        elif user_input == "4":
+            removeFile()
+        elif user_input == "5":
+            print("Closing filebook have a nice day!")
             isRunning = False
-welcomeLogg()
+    except Exception:
+        print("Invalid input")
